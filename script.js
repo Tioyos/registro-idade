26f0a831fecf10ce59465cc4ce4d6b3c895d6a4c
FELIPE DA SILVA CRUZ BRANDAO <felipe.scbrandao@senacsp.edu.br>
2024-09-24 15:06:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const nome = prompt("insira o seu nome:");
+
+let idade = parseInt(prompt("insira a sua idade")); 
+
+let anosparacem = 100 - idade
+alert(`${nome}, faltam ${anosparacem} anos para voce completar 100 anos`)
+
+
+
+
+
